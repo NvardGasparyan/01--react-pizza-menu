95c7bc099045c10dcfeffdd1d99db72acb8b3a5e
Nvard Gasparyan <ngvarsyan98@gmail.com>
2024-02-20 19:45:28 +0400

    pizza menu setup

---
+++ pizza-menu/src/components/Header/index.jsx
@@ -0,0 +1,10 @@
+const Header = () => {
+  const headingStyle = {};
+  return (
+    <header className="header">
+      <h1 style={headingStyle}>Fast React Pizza Co.</h1>
+    </header>
+  );
+};
+
+export default Header;
